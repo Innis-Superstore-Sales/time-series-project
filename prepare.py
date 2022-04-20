5c1e539b182604f18f241f55102a88555a90aed7
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-04-20 16:54:57 -0500

    Added separation by categories.
    In wrangle.ipynb added steps for separating the data by product category.
    
    In prepare.py added code for creating dataframes for each product
    category represented in the data. Also abstracted the various steps in
    the preparation process into different functions.

--- prepare.py
+++ prepare.py
@@ -2,15 +2,19 @@
 
     prepare.py
 
-    Description: Description
+    Description: This file contains functions used for preparing the superstore
+        data for exploration and modeling.
 
     Variables:
 
-        Variable
+        None
 
     Functions:
 
-        Function
+        prepare(df)
+        clean_columns(df)
+        separate_by_region(df)
+        separate_by_category(df)
 
 '''
 
@@ -20,13 +24,26 @@ import pandas as pd
 
 ################################################################################
 
-def prepare(df):
+def prepare(df: pd.DataFrame) -> pd.DataFrame:
+    df = clean_columns(df)
+    by_week = df.resample('W').sum()
+    east_by_week, west_by_week, central_by_week, south_by_week = separate_by_region(df)
+    office_supplies_by_week, furniture_by_week, technology_by_week = separate_by_category(df)
+
+    return by_week, east_by_week, west_by_week, central_by_week, south_by_week, office_supplies_by_week, furniture_by_week, technology_by_week
+
+################################################################################
+
+def clean_columns(df: pd.DataFrame) -> pd.DataFrame:
     df.columns = [column.lower().replace(' ', '_').replace('-','_') for column in df]
     df.order_date = pd.to_datetime(df.order_date)
     df = df.set_index('order_date').sort_index()
     df.drop(columns=['product_id', 'customer_id', 'region_id', 'category_id', 'order_id', 'postal_code', 'ship_date'], inplace=True)
-    by_week = df.resample('W').sum()
+    return df
 
+################################################################################
+
+def separate_by_region(df: pd.DataFrame) -> tuple[pd.DataFrame]:
     supertore_east = df[df.region_name == "East"]
     supertore_west = df[df.region_name == "West"]
     supertore_central = df[df.region_name == "Central"]
@@ -37,4 +54,17 @@ def prepare(df):
     central_by_week = supertore_central.resample('W').sum()
     south_by_week = supertore_south.resample('W').sum()
 
-    return by_week, east_by_week, west_by_week, central_by_week, south_by_week
+    return east_by_week, west_by_week, central_by_week, south_by_week
+
+################################################################################
+
+def separate_by_category(df: pd.DataFrame) -> tuple[pd.DataFrame]:
+    office_supplies = df[df.category == 'Office Supplies']
+    furniture = df[df.category == 'Furniture']
+    technology = df[df.category == 'Technology']
+
+    office_supplies_by_week = office_supplies.resample('W').sum()
+    furniture_by_week = furniture.resample('W').sum()
+    technology_by_week = technology.resample('W').sum()
+
+    return office_supplies_by_week, furniture_by_week, technology_by_week
