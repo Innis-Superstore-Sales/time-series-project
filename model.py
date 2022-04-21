bdac692aae0b512bdbd68e2d1b893a5033f7a027
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-04-21 11:17:04 -0500

    Added ensemble models.
    In model.ipynb added steps for producing various ensemble models.
    
    In model.py added functions used for producing ensemble models.

--- model.py
+++ model.py
@@ -26,13 +26,12 @@ from evaluate import *
 
 ################################################################################
 
-def establish_baseline(train, validate):
+def establish_baseline(train, validate, periods):
     forecast_values = {
         'Last Observed Value' : round(train.sales[-1], 2),
         'Simple Average' : round(train.sales.mean(), 2)
     }
 
-    periods = [4, 12, 26, 52]
     for period in periods:
         forecast_values[f'Moving Average {period} Weeks'] = round(train.sales.rolling(period).mean()[-1], 2)
 
@@ -45,3 +44,54 @@ def establish_baseline(train, validate):
         eval_df = append_eval_df(key, 'sales', validate, prediction, eval_df)
 
     return eval_df[eval_df.rmse == eval_df.rmse.min()]
+
+################################################################################
+
+def create_ensemble_predictions(trains, validates, target, strategy):
+    predictions = []
+
+    for train, validate in zip(trains, validates):
+        predictions.append(strategy(target, train, validate))
+
+    df = predictions[0]
+    for prediction in predictions[1 : ]:
+        df = df + prediction
+    return df
+
+################################################################################
+
+def simple_average_model(target, train, validate):
+    return make_static_predictions(target, train[target].mean(), validate.index)
+
+################################################################################
+
+def holt_model(target, train, validate):
+    model = Holt(train[target], damped_trend = True)
+    model = model.fit()
+    predictions =  model.predict(
+        start = validate.index[0],
+        end = validate.index[-1]
+    )
+    return pd.DataFrame(predictions, columns = [target], index = predictions.index)
+
+################################################################################
+
+def prophet_model(target, train, validate):
+    model = Prophet()
+    model.fit(pd.DataFrame({
+        'ds' : train.index,
+        'y' : train[target]
+    }))
+
+    future = model.make_future_dataframe(365)
+    results = model.predict(future)
+    predictions = results[['ds', 'yhat']].set_index('ds')
+    predictions.columns = [target]
+    return predictions.resample('Q').mean()
+
+################################################################################
+
+def previous_cycle_model(target, train, validate):
+    predictions = train.loc['2015'] + train.diff(4).mean()
+    predictions.index = validate.index
+    return predictions
